9ab480fe2871f516f07d31e7ffc639faa7f5949f
Keerthana <akeerthana1995@gmail.com>
2023-03-10 09:12:16 +0530

    Modified formula to use binaries directly and added version check in test

--- Formula/pvsadm.rb
+++ Formula/pvsadm.rb
@@ -1,18 +1,36 @@
 class Pvsadm < Formula
   desc "Tool for managing the IBM Power Systems Virtual Servers"
   homepage "https://github.com/ppc64le-cloud/pvsadm"
-  url "https://github.com/ppc64le-cloud/pvsadm/archive/refs/tags/v0.1.9.tar.gz"
-  sha256 "e94a3bd3dce57e8ec932f653b7a61909b4bf63a1af26eb6a9e8d690ff1be1adc"
   license "Apache-2.0"
+  version "v0.1.10"
 
-  depends_on "go" => :build
+  on_macos do
+    on_intel do
+        url 'https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.10/pvsadm-darwin-amd64.tar.gz'
+        sha256 '7f13efc579111124d36e06019c612c3e1de7accb44ded0bdaea8caf60f8ad799'
+    end
+
+    on_arm do
+      url "https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.10/pvsadm-darwin-arm64.tar.gz"
+      sha256 "135249849df05c106e8b2faa4074bebb92ea6557e1b143c343a97bd040808315"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url 'https://github.com/ppc64le-cloud/pvsadm/releases/download/v0.1.10/pvsadm-linux-amd64.tar.gz'
+      sha256 'cee82d4b5179fd04e736b820fc6c41877cf565f382512fce7a2e89762f99e909'
+    end
+
+  end
 
   def install
-    system "go", "build", *std_go_args, "-o", bin/"pvsadm"
+    bin.install "pvsadm"
   end
 
   test do
     output = shell_output("#{bin}/pvsadm get events 2>&1", 1)
     assert_match "Error: --instance-name or --instance-name required", output
+    assert_match "Version: #{version},", shell_output("#{bin}/pvsadm version |  awk '{print $1, $2}'")
   end
 end
